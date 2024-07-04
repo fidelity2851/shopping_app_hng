cf00e92e56262c6e74836da76fd5f617c96153ae
fidelity2851 <ukwuanifidelis2851@gmail.com>
2024-07-04 22:20:47 +0100

    minor refactor

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:shopping_app_hng/screens/order_success_screen.dart';
 
 class CheckoutScreen extends StatelessWidget {
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
   final List<Map<String, dynamic>> productList;
